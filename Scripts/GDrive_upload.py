25798ed2288eef5efc1cfe95ee1f5d7289586960
Raymart Orbita <raymart.orbita@infor.com>
2025-01-07 17:45:31 +0800

    updated the GDrive upload script , added new features and set folder

--- Scripts/GDrive_upload.py
+++ Scripts/GDrive_upload.py
@@ -1,12 +1,15 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.http import MediaFileUpload
-from gdrive_creds import SERVICE_ACCOUNT, FOLDER_ID
+from gdrive_creds import SERVICE_ACCOUNT
 import pyclip
 import os
+import sys
 from datetime import datetime
 from zipfile import ZipFile
-
+import tkinter as tk
+from tkinter import filedialog, messagebox, scrolledtext, simpledialog
+import shutil
 
 
 #Pre-requisite 
@@ -26,13 +29,35 @@ from zipfile import ZipFile
 # Define the scope and service account file path
 SCOPE = ['https://www.googleapis.com/auth/drive']
 SERVICE_ACCOUNT_FILE = SERVICE_ACCOUNT
-PARENT_FOLDER_ID= FOLDER_ID
+PARENT_FOLDER_ID = None
+
+
+log_path = os.getcwd() + "\logs"
+root = None
+
+
+def set_folder():
+    '''
+    Set the parent folder ID
+    '''
+
+    global PARENT_FOLDER_ID
+
+    new_parent_folder_id = simpledialog.askstring("Set Parent Folder ID", "Enter the parent folder ID:", initialvalue=PARENT_FOLDER_ID)
+
+    if new_parent_folder_id:
+        PARENT_FOLDER_ID = new_parent_folder_id
+        print(PARENT_FOLDER_ID)
+    else:
+        messagebox.showwarning("Error", "Parent folder ID not set.")
 
 
 
 # Create credentials using the service account file
 def create_credentials():
-
+    '''
+    Create credentials using the service account file
+    '''
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPE)
 
     return credentials
@@ -44,92 +69,182 @@ def write_log(file_to_upload):
     write a log entry when file is uploaded for audit
 
     '''
-    log_path = r"C:\RaymartFiles\Learning\Python\projects\devops\logs"
+    global log_path
     log_file = f"gdrive_upload.log"
-
 
     if not os.path.exists(log_path):
         os.makedirs(log_path)
 
     with open(f"{log_path}\{log_file}", "a") as log:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log_entry = f"{timestamp} - {file_to_upload} File uploaded"
+        log_entry = f"{timestamp} - {file_to_upload} File uploaded \n"
         log.write(log_entry)
 
 def zip_files(files_to_zip):
+    #get the current date and time
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    zip_full_name = f"{files_to_zip}_{timestamp}.zip"
-    
+
+    #create a temp directory
+    # temp_dir = r"C:\RaymartFiles\Learning\Python\projects\devops\Scripts\temp"
+    temp_dir = os.path.join(os.getcwd(), "temp")
+
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+        
+
+    #convert the path to a list and get the file name using os.path.basename 
+    file_name = [os.path.basename(files_to_zip)]
+
+    #name of the backup directory.
+    zip_full_name = f"{file_name[0]}_{timestamp}.zip"
+ 
+    #change the directory to the directory of the file
+    os.chdir(os.path.dirname(files_to_zip))
+
+    #iterate through the files and zip them
     with ZipFile(zip_full_name, "w") as zipf:
-        for file in files_to_zip:
+        for file in file_name:
             zipf.write(file)
+
+    try:
+         #move the zip file to the temp directory
+        shutil.copy(zip_full_name, temp_dir)
+    except shutil.Error:
+        print("An error occurred while moving the zip file to the temp directory.")
+        
 
     return zip_full_name
 
 
 
-def upload_file(file_path, file_name, parent_folder_id=None):
-
-    """
-    Uploads a file to Google Drive.
-
-    Args:
-        file_path (str): The path to the file to upload.
-        file_name (str): The name of the file to upload.
-        folder_id (str, optional): The ID of the folder to upload the file to. Defaults to None.
-    
-        
-    Returns:
-        The ID of the uploaded file.
-
-
-    """
-
-    creds = create_credentials()
-    service = build('drive', 'v3', credentials=creds)
-
-
-    file_metadate = {
-        'name': file_name,
-        'parents': [parent_folder_id]
-    }
-
-    # #zip the file before uploading to GDrive.
-    # if len(file_path) < 1:
-    #     print("File doesnt exist!")
-    # else:
-    #     file_to_upload = zip_files(file_path)
+def retrieve_url():
+    '''
+    Retrieve the Google Drive URL
+    '''
 
     try:
-        media = MediaFileUpload(file_path, mimetype='text/plain', resumable=True)
-
-        file = service.files().create(body=file_metadate, media_body=media, supportsAllDrives=True, fields='id').execute()
-        write_log(file_name)
-        #get the file id
-        file_id = file.get('id')
-
-        #for testing purposes.
-        print(f'File uploaded successfully.'  f'File ID: {file_id}')
-        
         #Google Drive URL
         FOLDER_PATH = f"https://drive.google.com/file/d/{file_id}/view"
+
+        display_window = tk.Toplevel()
+        display_window.title("GDrive URL")
+        display_message = tk.Label(display_window,text=f"Copy the link below to download the file", background="black", foreground="yellow")
+        display_message.pack()
+
+        text_area = scrolledtext.ScrolledText(display_window, wrap=tk.WORD, width=40, height=10)
+        text_area.insert(tk.END, FOLDER_PATH)
+        text_area.pack()
 
         #copy the file id to the clipboard to download
         pyclip.copy(FOLDER_PATH)
         print("Copied the download file URL.")
 
-    except Exception as e:
-        print(f"Error uploading file: {e}")
+    except Exception:
+        messagebox.showerror("Error", f"An error occurred: You need to Upload before retrieving the URL.")
+
+
+def upload_file():
+
+    """
+    Uploads a file to Google Drive.
+
+    """
+
+    global file_id
+
+    #open the file dialog and get the selected file
+    selected_file = filedialog.askopenfilename()
+
+    if selected_file:
+        if len(selected_file) > 1:
+            #zip the files
+            zip_file = zip_files(selected_file)
+            file_to_upload = zip_file
+        else:
+            file_to_upload = selected_file[0]
+
+    #create credentials
+    creds = create_credentials()
+
+    if not PARENT_FOLDER_ID:
+        messagebox.showerror("Please add the parent folder ID.", "Please add the parent folder ID before uploading a file.")
+
+    else:
+        #get the file name
+        uploaded_file_name = os.path.basename(file_to_upload)
+        #create drive api client
+        service = build('drive', 'v3', credentials=creds)
+        file_metadate = {
+            'name': uploaded_file_name,
+            'parents': [PARENT_FOLDER_ID]
+        }
+        
+        try:
+            media = MediaFileUpload(uploaded_file_name, mimetype='text/plain', resumable=True)
+
+            file = service.files().create(body=file_metadate, media_body=media, supportsAllDrives=True, fields='id').execute()
+            #create a log entry
+            write_log(uploaded_file_name)
+            #get the file id
+            file_id = file.get('id')
+            messagebox.showinfo("Success", f"File uploaded successfully. \nFile ID: {file_id}")
+        except Exception as e:
+            messagebox.showerror("Error", f"Error uploading file: {e}")
 
 
 
+def resource_path(relative_path):
+    '''
+    return the full path to image files in the project
 
+    '''
+    base_path = os.path.abspath(".")
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+def check_parent_folder():
+    if not PARENT_FOLDER_ID:
+        messagebox.showinfo("You need to set a parent folder ID.", "Please set a parent folder ID before uploading a file.")
+
+
+def gui():
+    '''
+    Gui for the program 
+
+    '''
+    
+    root = tk.Tk()
+    root.title("GDrive Upload Tool")
+    root.geometry("420x190")
+    root.eval("tk::PlaceWindow . center")
+    root.iconbitmap(resource_path("images\gdrive.ico"))
+    img = tk.PhotoImage(file=resource_path("images\gdrive.png"))
+    
+    parent_folder_button = tk.Button(root, image=img, text="Add Folder Name", command=set_folder)
+    parent_folder_button.pack(side=tk.LEFT, anchor='nw')
+
+    upload_button = tk.Button(root, text="Upload File", width=20, command=upload_file)
+    upload_button.pack(side=tk.TOP)
+
+    retrieve_button = tk.Button(root, text="Retrieve GDrive URL", width=20, command=retrieve_url)
+    retrieve_button.pack(side=tk.TOP)
+
+    close_button = tk.Button(root, text="Close", width=20, command=root.destroy)
+    close_button.pack(side=tk.TOP)
+
+    root.mainloop()
 
 
 def main():
 
+
     # Example usage:
-    upload_file(r"C:\Users\rorbita\Downloads\PR140.err", r"PR140.err", PARENT_FOLDER_ID)
+    gui()
 
 
 if __name__ == "__main__":
